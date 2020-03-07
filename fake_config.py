fdabdef5839468a8414a82ba157d41b0dd6528e8
Will Raphaelson <wraphaelson@ngpvan.com>
2020-03-07 15:20:58 -0500

    Update and rename config.py to fake_config.py

--- fake_config.py
+++ fake_config.py
@@ -1,4 +1,4 @@
-# this is fake, make your own that looks like this
+# this is fake, make your own that looks like this, and make it called config.py instead of fake_config.py.
 SLACK_BOT_TOKEN = "xxxx-xxxxxxxxxxxx-xxxxxxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxx"
 SLACK_APP_TOKEN = "xxxx-xxxxxxxxxxxx-xxxxxxxxxxxx-xxxxxxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 TEST_ENV = "GS6CFBN5N"
