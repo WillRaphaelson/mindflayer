01daf83651d5761f942b0f42db917a1325ed9515
Will Raphaelson <wraphaelson@ngpvan.com>
2020-03-07 15:19:28 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+# this is fake, make your own that looks like this
+SLACK_BOT_TOKEN = "xxxx-xxxxxxxxxxxx-xxxxxxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxx"
+SLACK_APP_TOKEN = "xxxx-xxxxxxxxxxxx-xxxxxxxxxxxx-xxxxxxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+TEST_ENV = "GS6CFBN5N"
+PROD_ENV = "CBHJ17SVC"
